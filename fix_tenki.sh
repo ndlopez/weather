73072d238eeb7ec014699970eb90e4bc63339577
Diego Lopez <lopez_diego@protonmail.com>
2020-09-30 12:43:51 +0900

    Update fix_tenki.sh
    Added an <if> statement to check whether  <data> folder exists.
    Uncommented line 18, since pandas outputs an error when reading the CSV file.

--- fix_tenki.sh
+++ fix_tenki.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-file1=data/get_tenki.csv #today
-file2=data/get_tenki2.csv #tomorrow
-rm $file1 $file2
+if [[ ! -d data ]];then
+   #create a data folder
+   mkdir data
+else
+   file1=data/get_tenki.csv #today
+   file2=data/get_tenki2.csv #tomorrow
+   rm $file1 $file2 #delete old data
+fi
 echo "Getting weather data..."
 python3.8 get_tenki.py
 if [[ -f $file1 ]]; then
@@ -10,7 +15,7 @@ if [[ -f $file1 ]]; then
     do
 	#fix data on file1
 	sed -i -e 's/(%),//; s=(m/s),==' $file$i
-	#sed -i -e 1's/.*/#&/' $file1 #either this or header=None in python
+	sed -i -e 1's/.*/#&/' $file$i
 	sed -i -e 3's/.*/気温,&/' $file$i
 	sed -i -e 6's/.*/降水量,&/' $file$i
 	rm $file$i-e
