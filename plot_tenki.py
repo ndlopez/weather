ebe306407ac6bd75b3d03682c248a61a0606f234
Diego Lopez <lopez_diego@protonmail.com>
2020-09-30 12:36:38 +0900

    Update plot_tenki.py
    Changed line 6: 
    Some error when parsing < header=None -> comment='#' >

--- plot_tenki.py
+++ plot_tenki.py
@@ -3,17 +3,16 @@ from datetime import date, timedelta
 import pandas as pd
 
 col_names=["時刻","天気","気温","降水確率","降水量","湿度","風速"]
-df = pd.read_csv('data/get_tenki.csv',header=None) #comment='#'
+df = pd.read_csv('data/get_tenki.csv',comment='#') #header=None doesn't WORK
 #the following line will delete the 0..6 indexing
 df.set_index(['時刻'],inplace=True)
 heute=date.today()
 df_tr = df.T
-#find a way to replace '---' with 0
-#df_tr = df_tr.replace('---',0)
+#df_tr = df_tr.replace('---',0) #SED already cleaned it
 print("Weather for:",heute)
 print(df_tr)
 print("Tomorrow's weather")
-df2 = pd.read_csv('data/get_tenki2.csv',header=None)
+df2 = pd.read_csv('data/get_tenki2.csv',comment='#')
 df2.set_index(['時刻'],inplace=True)
 
 morgen = heute + timedelta(1)
