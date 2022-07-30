0374252661831bd7c8068fb4ff61c841ef3937ff
ndlopez <ndzerglink@live.jp>
2022-07-30 13:24:16 +0900

    Delete all the things

--- webApp/myconfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dbpass="";
-$config = parse_ini_file('data/weather.ini');
-$conn = mysqli_connect($config['dbhost'], $config['dbuser'], $dbpass);
-mysqli_select_db($conn, $config['dbname']);
