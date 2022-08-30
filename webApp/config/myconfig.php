8c1e5bf5a11c70d043d7f27afb11c885a3389871
Diego Lopez <ndzerglink@live.jp>
2022-08-30 15:36:58 +0900

    moved webApp to new repo

--- webApp/config/myconfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-$dbpass="gWloF1Uk";
-$config = parse_ini_file('weather.ini');
-$conn = mysqli_connect($config['dbhost'], $config['dbuser'], $dbpass);
-mysqli_select_db($conn, $config['dbname']);
