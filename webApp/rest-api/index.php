9b758a726ac4b670f8aee74bb0c2560fb383c601
Diego Lopez <ndzerglink@live.jp>
2022-08-30 15:14:29 +0900

    Rest Api works fine now

--- webApp/rest-api/index.php
+++ webApp/rest-api/index.php
@@ -5,15 +5,16 @@ require __DIR__ . "/inc/bootstrap.php";
 
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/',$uri);
-
-if((isset($uri[2]) && $uri[2] != 'datos') || !isset($uri[3])){
-    header("HTTP/1.1 404 not found");
+var_dump("dasis:".$uri[4]);
+if((isset($uri[3]) && $uri[3] != 'datos') || !isset($uri[4])){
+    header("HTTP/1.1 404 not found :(");
+    //var_dump("Something 2:".$uri[3]."3:").$uri[4];
     exit();
 }
 
 require PROJECT_ROOT_PATH . "/control/dataControl.php";
 
 $objFeedController =  new DataController();
-$strMethodName = $uri[3] . 'Action';
+$strMethodName = $uri[4] . 'Action';
 $objFeedController->{$strMethodName}();
 ?>
