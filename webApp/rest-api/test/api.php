8714a860ee2715523485add618ca4b0610a12551
Diego Lopez <ndzerglink@live.jp>
2022-08-30 10:42:36 +0900

    500 error on fetching data from db

--- webApp/rest-api/test/api.php
+++ webApp/rest-api/test/api.php
@@ -2,19 +2,23 @@
 ini_set('display_errors',1);
 
 header("Content-Type:application/json");
-require "sample.php";
+//require "sample.php";
 
-//require "get_weather.php"
+require "get_weather.php"
 
-if(!empty($_GET['prod'])){
-    $name = $_GET['prod'];
-    $price = get_price($name);
-    //$got_data = index($myVal);
-    
-    if(empty($price)){
-        my_response(200,"Product not found",NULL);
+if(!empty($_GET['tag'])){
+    //prod: entry point
+    //$name = $_GET['prod'];
+    //$price = get_price($name);
+
+    $myVal = $_GET['tag']
+    //var_dump($myVal->format('Y-m-d'));
+    $got_data = index($myVal->format('Y-m-d'));
+
+    if(empty($got_data)){
+        my_response(200,"Data not found",NULL);
     }else{
-        my_response(200,"Product found",$price);
+        my_response(200,"Found data",$got_data);
     }
 }else{
     my_response(400,"Invalid request",NULL);
