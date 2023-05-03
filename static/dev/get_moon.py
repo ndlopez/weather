441031b299bd9f730afe82bc29e707613cd13729
Diego <ndzerglink@live.jp>
2023-05-03 22:24:24 +0900

    Edited moon times csv

--- static/dev/get_moon.py
+++ static/dev/get_moon.py
@@ -10,33 +10,38 @@ my_url='https://www.timeanddate.com/moon/japan/nagoya?month=' + \
     monty + '&year=' + year
 
 #when downloading a page...
-try:
+"""try:
     source=urlopen(my_url)
     print("Access granted")
 except HTTPError as err:
     print("Access denied or...", err.code)
-    print("Better use curl to fetch the page...")
-    source = open('../data/23108_3hr.html','r')
+    print("Better use curl to fetch the page...")"""
+    
+source = open('../../../MoonTimes.html','r')
 
 soup = BeautifulSoup(source.read(),'html.parser')
 
 def get_info(tab_id):
     tables = soup.find('table',id=tab_id)
     tab_tr = tables.find_all('tr')
-    
+    jdx=0
     for item in tab_tr:
         tab_th = item.find('th')
         tab_td = item.find_all(class_='sep') #pdr0
-        if tab_th is not None:
-            print(year,monty,"-",tab_th.string,end=',')
+        if tab_th is not None and jdx > 0:
+            aux = tab_th.string
+            if aux is None:
+                aux = jdx - 1
+            print(f"{year}-0{monty}-{aux}",end=';')
             for idx in range(len(tab_td)):
-                val_ery = tab_td[idx].string
-                if tab_td[0].string == "-":
-                    # tab_td[0] = 
-                    val_ery = tab_td[2].string
-                print(val_ery,end=',')
+                if tab_td[idx].string == "-":
+                    val_ery = tab_td[0].string
+                else:
+                    val_ery = tab_td[idx].string
+                print(val_ery,end=';')
             """for td_item in tab_td:
                 print(td_item.string,end=',')"""
             print()
+        jdx += 1
 
 get_info("tb-7dmn")
