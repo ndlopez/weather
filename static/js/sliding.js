76a601737b0049e67a11fb92fcdbea12c2cf73d2
Diego <ndzerglink@live.jp>
2024-04-05 22:20:58 +0900

    Yellow dust anim, dealt with next-day imgs

--- static/js/sliding.js
+++ static/js/sliding.js
@@ -1,34 +1,27 @@
-
-/*<link rel="stylesheet" type="text/css" href="../js/slideshow.css">
-
-<div class="slideshow-container" id="slides"></div>
-
-<div style="text-align:center">
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span> 
-  <span class="dot"></span>
-</div>*/
-
-let urls = "https://www.data.jma.go.jp/gmd/env/kosa/fcst/img/surf/jp/2024";
+/*Slideshow, courtesy of w3schools. Adapted for this site by DLopez. */
+// let urls = "https://www.data.jma.go.jp/gmd/env/kosa/fcst/img/surf/jp/2024";
 let slideIdx = 0;
-const kate = new Date();
+let kate = new Date();
 let heure = [0,3,6,9,12,15,18,21];
 const slid = document.getElementById("pm25_div");
 // slid.addEventListener("onclick",showSlides);
 let txt = "",jdx;
 let amy = "", lena = "";
+if (kate.getHours() > 21){
+  //display next day forecast
+  let tody = new Date(jahre+"-"+monty+"-"+tag);//vars def on build_data.js
+  let morrow = new Date(tody);
+  let aux = morrow.setDate(tody.getDate()+1)
+  kate = new Date(aux);
+  // myIdx = 0;
+}
 for (let idx in heure){
     jdx = String(heure[idx]);
     if (heure[idx] <10) jdx = "0" + String(heure[idx]);
     if (heure[idx] > 0){lena = "mySlides";}
-    txt += `<div id="slide${idx}" class=${lena}><img src="${urls}0${kate.getMonth()+1}0${kate.getDate()}${jdx}00_kosafcst-s_jp_jp.png" style="width:100%">` //</div>
+    txt += `<div id="slide${idx}" class=${lena}>
+    <img src="${pm25_url}${jahre}0${kate.getMonth()+1}0${kate.getDate()}${jdx}00_kosafcst-s_jp_jp.png" style="width:100%">` //</div>
     amy += "<span class='dot'></span>";
-    // <div class="numbertext">${jdx}/${String(heure.length)}</div>
     txt += `<div class="text_kate">2024-0${kate.getMonth()+1}-0${kate.getDate()} ${jdx}:00</div></div>`;
 }
 amy = "<div style='text-align:center'>" + amy + "</div>";
@@ -49,5 +42,5 @@ function showSlides() {
   }
   slides[slideIdx-1].style.display = "block";  
   dots[slideIdx-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+  setTimeout(showSlides, 3000);
 }
