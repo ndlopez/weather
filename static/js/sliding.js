f82ba6f67a36ce2a36dcc98d4ec0d47ee4149491
Diego <ndzerglink@live.jp>
2024-04-05 21:39:13 +0900

    Added date to yellow dust anim

--- static/js/sliding.js
+++ static/js/sliding.js
@@ -14,27 +14,28 @@
   <span class="dot"></span>
 </div>*/
 
-let urls="https://www.data.jma.go.jp/gmd/env/kosa/fcst/img/surf/jp/20240403";
+let urls = "https://www.data.jma.go.jp/gmd/env/kosa/fcst/img/surf/jp/2024";
 let slideIdx = 0;
-
+const kate = new Date();
 let heure = [0,3,6,9,12,15,18,21];
-const slid=document.getElementById("pm25_div");
+const slid = document.getElementById("pm25_div");
 // slid.addEventListener("onclick",showSlides);
-let txt="",jdx;
-let amy = "";
+let txt = "",jdx;
+let amy = "", lena = "";
 for (let idx in heure){
     jdx = String(heure[idx]);
     if (heure[idx] <10) jdx = "0" + String(heure[idx]);
-    txt += `<div class="mySlides">
-    <img src="${urls}${jdx}00_kosafcst-s_jp_jp.png" style="width:100%"></div>`
+    if (heure[idx] > 0){lena = "mySlides";}
+    txt += `<div id="slide${idx}" class=${lena}><img src="${urls}0${kate.getMonth()+1}0${kate.getDate()}${jdx}00_kosafcst-s_jp_jp.png" style="width:100%">` //</div>
     amy += "<span class='dot'></span>";
     // <div class="numbertext">${jdx}/${String(heure.length)}</div>
-    // <div class="text">2024-03-31 ${jdx}:00</div>
+    txt += `<div class="text_kate">2024-0${kate.getMonth()+1}-0${kate.getDate()} ${jdx}:00</div></div>`;
 }
 amy = "<div style='text-align:center'>" + amy + "</div>";
 slid.innerHTML = txt + amy;
 
 function showSlides() {
+  document.getElementById("slide0").classList.add("mySlides");
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
